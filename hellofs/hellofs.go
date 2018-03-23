abac0e23416b29eea919c04388acc9d670ceb478
gargrohin <gargrohin0@gmail.com>
2018-03-23 09:02:01 +0530

    initial write atempts in hellofs

--- hellofs/hellofs.go
+++ hellofs/hellofs.go
@@ -9,30 +9,31 @@ import (
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 	"golang.org/x/net/context"
+	_ "io/ioutil"
 )
 
-/*func usage() {
+func usage() {
 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
 	fmt.Fprintf(os.Stderr, "  %s ROOT MOUNTPOINT\n", os.Args[0])
 	flag.PrintDefaults()
-}*/
+}
 
 func main() {
-	//flag.Usage = usage()
+	flag.Usage = usage
 	//mountpoint := flag.String("mp", "", "a directory to mount the fs on")
 	flag.Parse()
 
 	mountpoint := flag.Arg(0)
 
 	if flag.NArg() != 1 {
-		//usage()
+		usage()
 		fmt.Println("no mountpoint provided")
 		os.Exit(2)
 	}
 
-	hw := &File{"hw", "hello world!", 1, 0777}
-	inode := &File{"inode", "inode works how?", 2, 0777}
-	root := &Dir{"test", 2, 3, nil, hw, inode}
+	hw := &File{"hw", "hello world!\n", 1, 0777}
+	//inode := &File{"inode", "inode works how?\n", 2, 0777}
+	root := &Dir{"test", 2, 3, nil, hw, nil}
 	myfs := &FS{"hellofs", root}
 
 	con, err := fuse.Mount(
@@ -82,9 +83,9 @@ type Dir struct {
 }
 
 func (d Dir) Attr(ctx context.Context, a *fuse.Attr) error {
-	a.Inode = 0
+	a.Inode = d.inode
 	a.Mode = os.ModeDir | 0777
-	d.files = 2
+	//d.files = 2
 	return nil
 }
 
@@ -97,7 +98,7 @@ func (d Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
 	if d.nextdir != nil && name == d.nextdir.name {
 		return *(d.nextdir), nil
 	}
-	if d.nextfile2 != nil && name == d.nextfile2.name {
+	if d.nextfile2 != nil {
 		return *(d.nextfile2), nil
 	}
 	return nil, fuse.ENOENT
@@ -123,6 +124,21 @@ func (d Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 
 }
 
+func (d Dir) MkDir(ctx context.Context, req *fuse.MkdirRequest) (fs.Node, error) {
+	dir := &Dir{name: req.Name, files: 0, inode: 10, nextdir: nil, nextfile: nil, nextfile2: nil}
+	d.nextdir = dir
+	return dir, nil
+}
+
+func (d Dir) Create(ctx context.Context, req *fuse.CreateRequest, resp *fuse.CreateResponse) (fs.Node, fs.Handle, error) {
+	f := &File{name: req.Name, text: "woah\n", inode: uint64(resp.LookupResponse.Node), osmode: req.Mode}
+	//f, err := os.Create(req.Name)
+
+	//b, _ := ioutil.ReadFile(f.Name())
+	d.nextfile2 = f
+	return d.nextfile2, d.nextfile2, nil
+}
+
 //File handlers
 
 //const text = "Hello, World!\n"
@@ -133,6 +149,16 @@ type File struct {
 	inode  uint64
 	osmode os.FileMode
 }
+
+func (d Dir) Write(ctx context.Context, req fuse.WriteRequest, resp fuse.WriteResponse) error {
+	resp.Size = (len(d.nextfile.text) + len(req.Data))
+	d.nextfile.text = d.nextfile.text + string(req.Data)
+	return nil
+}
+
+/*func (f File) Rename(ctx context.Context, req *fuse.RenameRequest, newDir fs.Node) error {
+
+}*/
 
 func (f File) Attr(ctx context.Context, a *fuse.Attr) error {
 	a.Inode = f.inode
@@ -145,3 +171,13 @@ func (f File) Attr(ctx context.Context, a *fuse.Attr) error {
 func (f File) ReadAll(ctx context.Context) ([]byte, error) {
 	return []byte(f.text), nil
 }
+
+func (f File) Write(ctx context.Context, req fuse.WriteRequest, resp fuse.WriteResponse) error {
+	resp.Size = (len(f.text) + len(req.Data))
+	f.text = f.text + string(req.Data)
+	return nil
+}
+
+/*func (f File) Fsync(ctx context.Context, req *fuse.FsyncRequest) error {
+	return nil
+}*/
---
+++ hellofs/tests/helloworld_test.go
@@ -0,0 +1,71 @@
+package foo
+
+import (
+	"bazil.org/fuse"
+	"golang.org/x/net/context"
+	"os"
+	"testing"
+)
+
+func TestMount(t *testing.T) {
+	/*flag.Parse()
+
+	if flag.NArg() != 1 {
+		t.Error("no mountpoint given")
+	}
+
+	mountpoint := flag.Arg(0)*/
+	mountpoint := "mnt"
+
+	out := mount(mountpoint)
+
+	if out != nil {
+		t.Error("error mounting the fs")
+	}
+
+}
+
+var fhandle FS
+
+func TestRoot(t *testing.T) {
+	err := fhandle.Root()
+	if err != nil {
+		t.Error("root returning error")
+	}
+}
+
+var dir Dir
+var ctx context.Context
+var f File
+
+func TestDirAttr(t *testing.T) {
+
+	d, a, err := dir.Attr(ctx)
+
+	if err != nil || d.Files != 2 || a.Mode != os.ModeDir|0777 {
+		t.Error("error in Directory's attributes")
+	}
+
+	a, err = f.Attr(ctx)
+
+	if err != nil || a.Size != uint64(len(text)) || a.Mode != 0777 {
+		t.Error("error in File's attributes")
+	}
+
+}
+
+func TestReadDir(t *testing.T) {
+
+	rootchild, err := dir.ReadDirAll(ctx)
+
+	if rootchild[0].Type != fuse.DT_File || rootchild[0].Name != "" || err != nil {
+		t.Error("error in links between root and children")
+	}
+
+	txt, err := f.ReadAll(ctx)
+
+	if err != nil || text != string(txt[:]) {
+		t.Error("error in File's contents")
+	}
+
+}
